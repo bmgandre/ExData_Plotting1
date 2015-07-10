09b0ae2f8e9cd71c966dd188f788d8887a9a9e8c
André B. M. Gomes <bmg.andre@gmail.com>
2015-07-10 15:06:38 -0300

    Update plot1 to filter using the same strategy of plot[2-4]

--- plot1.R
+++ plot1.R
@@ -2,29 +2,24 @@ input <- "../household_power_consumption.txt"
 in_sep <- ";"
 in_na <- "?"
 in_dateFormat <- "%d/%m/%Y"
-in_colClasses = c("myDate", "character", rep('numeric', 7))
+in_colClasses = c("character", "character", rep('numeric', 7))
 
 output <- "plot1.png"
 out_width <- 480
 out_height <- 480
 
-setClass("myDate")
-setAs("character", "myDate",
-      function(from) { 
-          as.Date(from, format=in_dateFormat)
-      });
-
 data <- read.csv(file=input, header=T, sep=in_sep, 
                  na.string=in_na, colClasses=in_colClasses)
 
+col_filter1 <- c("Date", "Global_active_power")
+data <- data[, col_filter1]
+data$Date <- as.Date(data$Date, format=in_dateFormat)
+
 date_min = as.Date("2007-02-01")
-date_max = as.Date("2007-02-02")
+date_max = as.Date("2007-02-03")
 
-date_filter = c(date_min, date_max)
-row_filter <- data$Date %in% date_filter
-col_filter <- "Global_active_power"
-
-data <- data[row_filter, col_filter]
+row_filter <- (data$Date >= date_min) & (data$Date < date_max)
+data <- data[row_filter, ]
 
 graph_title <- "Global Active Power"
 graph_x_title <- "Global Active Power (kilowatts)"
@@ -32,5 +27,7 @@ graph_y_title <- "Frequency"
 graph_col_color <- 552 #red
 
 png(filename=output, width=out_width, height=out_height)
-hist(data, main=graph_title, xlab=graph_x_title, ylab=graph_y_title, col="red")
+hist(data$Global_active_power, 
+     main=graph_title, xlab=graph_x_title, ylab=graph_y_title, 
+     col="red")
 dev.off()
